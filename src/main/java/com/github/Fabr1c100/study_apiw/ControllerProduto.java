279a7d64859b63fa0156b624eac21c6cf1686d5d
F@br1c100 <rm97631@fiap.com.br>
2025-03-10 20:54:38 -0300

    Update com Swagger e Dependências OpenAPI

--- src/main/java/com/github/Fabr1c100/study_apiw/ControllerProduto.java
+++ src/main/java/com/github/Fabr1c100/study_apiw/ControllerProduto.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ControllerProduto {
 
     @PostMapping
-    public ResponseEntity<String> crete(){
+    public ResponseEntity<String> crete(@RequestBody Produto produto){
         return ResponseEntity.status(201).body("produto cadastrado: maça");
     }
 
